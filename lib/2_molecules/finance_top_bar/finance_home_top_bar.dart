066d7da465ad3235eb9a6f7e48f4f7147969554c
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-16 17:01:37 -0300

    app bar

--- lib/2_molecules/finance_top_bar/finance_home_top_bar.dart
+++ lib/2_molecules/finance_top_bar/finance_home_top_bar.dart
@@ -2,14 +2,14 @@ import 'package:finance_control_ui/finance_control_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class FinanceHomeTopBar extends StatelessWidget {
+class FinanceHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
   const FinanceHomeTopBar({
-    super.key,
+    Key? key,
     required this.addRoute,
     required this.removeRoute,
     required this.transactionRoute,
     required this.menuRoute,
-  });
+  }) : super(key: key);
 
   final Function() addRoute;
   final Function() removeRoute;
@@ -18,66 +18,63 @@ class FinanceHomeTopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 254,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        color: AppColors.deepBlue,
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            right: 20,
-            child: SvgPicture.asset('assets/icons/lines.svg'),
-          ),
-          Positioned(
-            left: 20,
-            bottom: 147,
-            child: FinanceText.h3(
+    return AppBar(
+      backgroundColor: AppColors.deepBlue,
+      titleSpacing: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(bottom: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FinanceText.h3(
               'Saldo',
               color: AppColors.white,
             ),
-          ),
-          Positioned(
-            left: 20,
-            bottom: 127,
-            child: FinanceText.p14(
+            FinanceText.p14(
               'Saldo',
               color: AppColors.white.withOpacity(0.5),
             ),
+          ],
+        ),
+      ),
+      actions: [
+        SvgPicture.asset('assets/icons/lines.svg', fit: BoxFit.fitHeight),
+      ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FinanceMenu(
+                label: 'Adicionar',
+                icon: Icons.add_rounded,
+                onTap: addRoute,
+              ),
+              FinanceMenu(
+                label: 'Remover',
+                icon: Icons.remove_rounded,
+                onTap: removeRoute,
+              ),
+              FinanceMenu(
+                label: 'Transações',
+                icon: Icons.receipt_outlined,
+                onTap: transactionRoute,
+              ),
+              FinanceMenu(
+                label: 'Menu',
+                icon: Icons.dehaze_outlined,
+                onTap: menuRoute,
+              ),
+            ],
           ),
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 16,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FinanceMenu(
-                  label: 'Adicionar',
-                  icon: Icons.add_rounded,
-                  onTap: addRoute,
-                ),
-                FinanceMenu(
-                  label: 'Remover',
-                  icon: Icons.remove_rounded,
-                  onTap: removeRoute,
-                ),
-                FinanceMenu(
-                  label: 'Transações',
-                  icon: Icons.receipt_outlined,
-                  onTap: transactionRoute,
-                ),
-                FinanceMenu(
-                  label: 'Menu',
-                  icon: Icons.dehaze_outlined,
-                  onTap: menuRoute,
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
+
+  @override
+  Size get preferredSize =>
+      const Size.fromHeight(254); // total height of the AppBar
 }
