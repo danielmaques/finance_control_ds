30d5e5980a7d2e87703dd44ff5a8f5a74e1b0658
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-16 16:00:47 -0300

    divider

--- lib/2_molecules/finance_credt_card_tile/finance_credt_card_tile.dart
+++ lib/2_molecules/finance_credt_card_tile/finance_credt_card_tile.dart
@@ -44,12 +44,9 @@ class FinanceCredtCardTile extends StatelessWidget {
             itemCount: 3,
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            separatorBuilder: (context, index) => SizedBox(
-              height: 20,
-              child: Divider(
-                height: 1,
-                color: Colors.blueGrey[100],
-              ),
+            separatorBuilder: (context, index) =>  Divider(
+              height: 1,
+              color: Colors.blueGrey[100],
             ),
             itemBuilder: (context, index) {
               return Row(
