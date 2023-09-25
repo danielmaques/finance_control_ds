e3de93499e21abb73df150750c0f7a7d61a256ac
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-25 15:15:13 -0300

    SliverToBoxAdapter

--- lib/2_molecules/finance_top_bar/finance_home_top_bar.dart
+++ lib/2_molecules/finance_top_bar/finance_home_top_bar.dart
@@ -1,17 +1,18 @@
-import 'package:finance_control_ui/finance_control_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class FinanceHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
-  const FinanceHomeTopBar({
-    super.key,
+import '../../finance_control_ui.dart';
+
+class FinanceHomeTopBarSliver extends StatelessWidget {
+  const FinanceHomeTopBarSliver({
+    Key? key,
     required this.addRoute,
     required this.removeRoute,
     required this.transactionRoute,
     required this.menuRoute,
     required this.money,
-  });
+  }) : super(key: key);
 
   final Function() addRoute;
   final Function() removeRoute;
@@ -21,72 +22,77 @@ class FinanceHomeTopBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 254,
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        color: AppColors.deepBlue,
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            right: 0,
-            child: SvgPicture.asset('assets/icons/lines.svg'),
-          ),
-          Positioned(
-            left: 20,
-            bottom: 147,
-            child: ValueListenableBuilder(
-              valueListenable: money,
-              builder: (context, value, child) => FinanceText.h3(
-                formatMoney(value),
-                color: AppColors.white,
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 254,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: AppColors.deepBlue,
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              child: SvgPicture.asset('assets/icons/lines.svg'),
+            ),
+            Positioned(
+              left: 20,
+              bottom: 147,
+              child: ValueListenableBuilder(
+                valueListenable: money,
+                builder: (context, value, child) => Text(
+                  formatMoney(value),
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            left: 20,
-            bottom: 127,
-            child: FinanceText.p14(
-              'Saldo',
-              color: AppColors.white.withOpacity(0.5),
+            Positioned(
+              left: 20,
+              bottom: 127,
+              child: Text(
+                'Saldo',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white.withOpacity(0.5),
+                ),
+              ),
             ),
-          ),
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 16,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FinanceMenu(
-                  label: 'Adicionar',
-                  icon: Icons.add_rounded,
-                  onTap: addRoute,
-                ),
-                FinanceMenu(
-                  label: 'Remover',
-                  icon: Icons.remove_rounded,
-                  onTap: removeRoute,
-                ),
-                FinanceMenu(
-                  label: 'Transações',
-                  icon: Icons.receipt_outlined,
-                  onTap: transactionRoute,
-                ),
-                FinanceMenu(
-                  label: 'Menu',
-                  icon: Icons.dehaze_outlined,
-                  onTap: menuRoute,
-                ),
-              ],
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 16,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FinanceMenu(
+                    label: 'Adicionar',
+                    icon: Icons.add_rounded,
+                    onTap: addRoute,
+                  ),
+                  FinanceMenu(
+                    label: 'Remover',
+                    icon: Icons.remove_rounded,
+                    onTap: removeRoute,
+                  ),
+                  FinanceMenu(
+                    label: 'Transações',
+                    icon: Icons.receipt_outlined,
+                    onTap: transactionRoute,
+                  ),
+                  FinanceMenu(
+                    label: 'Menu',
+                    icon: Icons.dehaze_outlined,
+                    onTap: menuRoute,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(254);
 }
