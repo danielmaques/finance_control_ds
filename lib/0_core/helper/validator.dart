bc95224bd7cc26ab8fe3b704d5e37c02bd63a4ef
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-09-30 11:46:22 -0300

    validadator

---
+++ lib/0_core/helper/validator.dart
@@ -0,0 +1,8 @@
+bool isEmailValid(String email) {
+  final RegExp emailRegex = RegExp(
+    r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$',
+    caseSensitive: false,
+  );
+
+  return emailRegex.hasMatch(email);
+}
