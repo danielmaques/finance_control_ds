c4d29de1f7226c4c8dc7347cb31827b0f8931bfc
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-10-14 13:02:58 -0300

    update finance_transaction_list.dart

--- lib/2_molecules/finance_transaction_list/finance_transaction_list.dart
+++ lib/2_molecules/finance_transaction_list/finance_transaction_list.dart
@@ -7,125 +7,97 @@ class FinanceTransactionList extends StatelessWidget {
     required this.transactionsByMonth,
   });
 
-  final Map<String, List<Map<String, dynamic>>> transactionsByMonth;
+  final List<dynamic> transactionsByMonth;
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: transactionsByMonth.keys.length,
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      separatorBuilder: (context, index) => const SizedBox(height: 20),
+      itemCount: transactionsByMonth.length,
+      separatorBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.symmetric(vertical: 14),
+        child: Divider(color: Colors.black26),
+      ),
       itemBuilder: (context, index) {
-        String month = transactionsByMonth.keys.elementAt(index);
-        List<Map<String, dynamic>> transactionsForMonth =
-            transactionsByMonth[month]!;
-
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Cabeçalho do mês
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 14),
-              child: FinanceText.p16(
-                month,
-                color: const Color(0xFF717E95),
-              ),
-            ),
-            ListView.separated(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: transactionsForMonth.length,
-              separatorBuilder: (context, index) => const Padding(
-                padding: EdgeInsets.symmetric(vertical: 14),
-                child: Divider(color: Colors.black26),
-              ),
-              itemBuilder: (context, index) {
-                final currentTransaction = transactionsForMonth[index];
-
-                return GestureDetector(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          actionsAlignment: MainAxisAlignment.center,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          content: Column(
-                            mainAxisSize: MainAxisSize
-                                .min, // Para limitar o tamanho do AlertDialog ao conteúdo
-                            children: [
-                              FinanceText.p18(currentTransaction['categoria']),
-                              const SizedBox(height: 10),
-                              FinanceText.h4(
-                                formatMoney(currentTransaction['valor']),
-                                color: currentTransaction['add'] == true
-                                    ? AppColors.forestGreen
-                                    : AppColors.cherryRed,
-                              ),
-                              const SizedBox(height: 10),
-                              FinanceText.p16(currentTransaction['descricao']),
-                              FinanceText.p14(currentTransaction['pagamento']),
-                              FinanceText.p12(formatDate(
-                                  currentTransaction['data'].toDate())),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  child: Container(
-                    color: Colors.transparent,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 48,
-                          width: 48,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFEEF2F8),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Center(
-                            child: FinanceText.p18(
-                              getInitial(currentTransaction['descricao']),
-                              color: AppColors.deepBlue,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 15),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              FinanceText.p16(
-                                currentTransaction['descricao'],
-                                color: Colors.black,
-                              ),
-                              FinanceText.p16(
-                                formatDate(currentTransaction['data'].toDate()),
-                                color: const Color(0xFF717E95),
-                              ),
-                            ],
-                          ),
-                        ),
-                        FinanceText.p18(
-                          formatMoney(currentTransaction['valor']),
-                          color: currentTransaction['add'] == true
-                              ? AppColors.forestGreen
-                              : AppColors.cherryRed,
-                        ),
-                      ],
-                    ),
+        return GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  actionsAlignment: MainAxisAlignment.center,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FinanceText.p16(transactionsByMonth[index].category),
+                      const SizedBox(height: 10),
+                      FinanceText.p18(
+                        formatMoney(transactionsByMonth[index].value ?? 0),
+                        color: transactionsByMonth[index].add == true
+                            ? AppColors.forestGreen
+                            : AppColors.cherryRed,
+                      ),
+                      const SizedBox(height: 10),
+                      FinanceText.p14(transactionsByMonth[index].description),
+                      FinanceText.p12(transactionsByMonth[index].method),
+                      FinanceText.p12(
+                          formatDate(transactionsByMonth[index].time!)),
+                    ],
                   ),
                 );
               },
+            );
+          },
+          child: Container(
+            color: Colors.transparent,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFEEF2F8),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: FinanceText.p16(
+                      getInitial(transactionsByMonth[index].description),
+                      color: AppColors.deepBlue,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FinanceText.p14(
+                        transactionsByMonth[index].description,
+                        color: Colors.black,
+                      ),
+                      FinanceText.p14(
+                        formatDate(transactionsByMonth[index].time!),
+                        color: const Color(0xFF717E95),
+                      ),
+                    ],
+                  ),
+                ),
+                FinanceText.p16(
+                  formatMoney(transactionsByMonth[index].value ?? 0),
+                  color: transactionsByMonth[index].add == true
+                      ? AppColors.forestGreen
+                      : AppColors.cherryRed,
+                ),
+              ],
             ),
-          ],
+          ),
         );
       },
     );
